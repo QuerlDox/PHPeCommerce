84b6fc92cf9ef864b893ff0761db4babde5d701e
rhelDeveloperFullName <rhelDeveloper@RHELdeveloper.localdomain>
2018-07-15 21:25:06 +0800

    created home page upto Header

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Harold's Boutique</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<!-- Top Nav Bar-->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<a href="index.php" class="navbar-brand">Harold's Boutique</a>
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Shirts</a></li>
+							<li><a href="#">Pants</a></li>
+							<li><a href="#">Shoes</a></li>
+							<li><a href="#">Accessories</a></li>
+						</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
+
+	<!-- Header -->
+	<div id="headerWrapper">
+		<div id="header-background"></div>
+		<div id="logotext"></div>
+	</div>
+   
+   
+	<p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula ante in sem feugiat, sit amet laoreet quam sagittis. Vestibulum pellentesque, ipsum sed hendrerit volutpat, dolor est mollis enim, vel eleifend metus odio eget sem. Vivamus vitae massa auctor, aliquam mi non, maximus velit. Cras cursus elit a turpis viverra malesuada sagittis in enim. 
+      Nullam pretium iaculis massa, eu condimentum nisi aliquet at. In quis ultricies sem. Integer placerat sit amet arcu a volutpat. Proin imperdiet justo et sollicitudin dictum. Fusce eget ullamcorper arcu. Praesent sed rhoncus nisi, eget auctor purus. Vivamus ac posuere ante. Donec quis scelerisque dolor, vitae volutpat enim. Duis egestas nulla velit, ac egestas neque ornare at. Aliquam eu ante tortor. Cras sit amet sapien sapien.
+
+	</p>
+
+</body>
+</html>
