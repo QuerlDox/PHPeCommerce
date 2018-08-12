f52f645fa7878fe0e3287c85377a02ab28d3d8de
rhelDeveloperFullName <rhelDeveloper@RHELdeveloper.localdomain>
2018-08-12 10:28:43 +0800

    Dynamically programmed the Modal

--- includes/detailsmodal.php
+++ includes/detailsmodal.php
@@ -1,4 +1,20 @@
+
+<?php
+	require_once '../core/init.php';
+	$id = $_POST['id'];
+	$id = (int)$id;
+	$sql = "SELECT * FROM products WHERE id = '$id'";
+	$sql_query = $db -> query($sql);
+	$modal_product = mysqli_fetch_assoc($sql_query);
+	$brand_id = (int)$modal_product['brand'];
+	$sql = "SELECT * FROM brand WHERE id = '$brand_id'";
+	$sql_query = $db -> query($sql);
+	$modal_brand = mysqli_fetch_assoc($sql_query);
+	$sizes_column = $modal_product['sizes'];
+	$sizes_array = explode(',', $sizes_column);
+?>
 <?php ob_start();?>
+
 <div class="modal fade details-modal" id="details-modal" tabindex="-1" role="dialog" aria-labelledby="details-modal" aria-hidden="true">
 		<div class="modal-dialog modal-lg">
 		<div class="modal-content">	
@@ -6,7 +22,7 @@
 				<button class="close" type="button" data-dismiss="modal" aria-label="close">
 					<span aria-hidden="true">&times;</span>
 				</button>
-				<h4 class="modal-title text-center">Levi's Jeans</h4>
+				<h4 class="modal-title text-center"><?php echo $modal_product['title']?></h4>
 			</div>
 			<!-- end of modal-header-->
 
@@ -15,15 +31,17 @@
 					<div class="row">
 						<div class="col-sm-6">
 							<div class="center-block">
-								<img src="images/products/men4.png" alt="Levi's Jeans" class="details img-responsive"/>
+								<img src="<?php echo $modal_product['image']?>" alt="<?php echo $modal_product['title']?> class="details img-responsive"/>
 							</div>
 						</div>
 						<div class="col-sm-6">
 							<h4>Details</h4>
-							<p>These Jeans are amazing.Get it now!</p>
+							<p><?php echo $modal_product['description']?></p>
 							<hr>
-							<p>Price: $34.99</p>
-							<p>Brand: Levis</p>
+							<p><?php echo $modal_product['list_price']?></p>
+							<p>Brand: <?php echo $modal_brand['brand']?></p>
+
+							
 							<form action="add_cart.php" method="post">
 								<div class="form-group">
 									<div class="col-xs-3">
@@ -35,12 +53,18 @@
 							<br><br>
 							<div class="form-group">
 								<label for="size">Size:</label>
-								<select name="size" id="size" class="form-control">
-									<option value=""></option>
-									<option value="28">28</option>
-									<option value="32">32</option>
-									<option value="36">36</option>
-								</select>	
+								 <select name="size" id="size" class="form-control">
+									<?php foreach($sizes_array as $sizes_array_element){
+									   $sizes_entry = explode(':', $sizes_array_element);
+									   $size = $sizes_entry[0];
+									   $quantity = $sizes_entry[1];	
+								
+									echo'<option value=".$size.">'.$size.'('.$quantity.' Available)</option>';
+									
+									
+
+									} ?>
+							</select>		
 							</div>	
 							</form>	
 						</div>
@@ -51,11 +75,20 @@
 			<!-- End of modal-body-->
 
 			<div class="modal-footer">
-				<button class="btn btn-default" data-dismiss="modal">Close</button>
+				<button class="btn btn-default" onclick="closeModal()">Close</button>
 				<button class="btn btn-warning" type="submit"><span class="glyphicon glyphicon-shopping-cart"></span>Add to Cart</button>
 			</div><!-- end of modal-footer-->
 
 		</div><!-- end of modal content-->	
+		<script type="text/javascript">
+			function closeModal(){
+				jQuery('#details-modal').modal('hide');
+				setTimeout(function(){
+					jQuery('#details-modal').remove();
+					jQuery('.modal-backdrop').remove();
+				},500);
+			}
+		</script>
 		</div>
 	</div>
 <?php echo ob_get_clean(); ?>
